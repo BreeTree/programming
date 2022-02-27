27f9018349b87f410dbe79250b6be66c6b403620
Breeda Herlihy <G00411369@gmit.ie>
2022-02-27 09:26:19 +0000

    Week04

---
+++ Week04/isEven.py
@@ -0,0 +1,8 @@
+# Program to check if number is even or odd
+# Author: Breeda Herlihy
+
+number = int(input("Enter a number:"))
+if (number % 2) == 0:
+    print ("{} is an even number".format(number))
+else:
+    print ("{} is an odd number".format(number))
